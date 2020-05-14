20fbd8421e7dff9181153944b7bdf17ca77fc5c5
Garrett <garrett56x@gmail.com>
2020-05-14 15:45:08 -0700

    Removed unused code

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import './Home.scss';
+import './Home.scss';
 
 class Home extends React.Component {
     componentDidMount() {
